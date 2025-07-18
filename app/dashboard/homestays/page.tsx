7b5c6d397185b9aeb88540d7e4e688e81ba0b771
Marcelo Surco <surcos2002@gmail.com>
2025-07-18 03:27:43 -0500

    fix error on build

--- app/dashboard/homestays/page.tsx
+++ app/dashboard/homestays/page.tsx
@@ -1,19 +1,11 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { HomestaysTable } from './homestays-table';
 
-export default async function HomestaysPage(
-  props: {
-    searchParams: Promise<{ q: string; offset: string }>;
-  }
-) {
-  const newOffset = 0;
-
+export default async function HomestaysPage() {
   return (
     <Tabs defaultValue="all">
       <TabsContent value="all">
-        <HomestaysTable
-          offset={newOffset ?? 0}
-        />
+        <HomestaysTable />
       </TabsContent>
     </Tabs>
   );
